6ba41865a9541beedd593898bcfe1b307c79bdb3
Hieu Nguyen <hieunguyen2287@gmail.com>
2022-11-17 23:18:18 +0700

    user

---
+++ db/sqlc/user_test.go
@@ -0,0 +1,52 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/harryng22/simplebank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func CreateRandomUser(t *testing.T) User {
+	arg := CreateUserParams{
+		Username:       util.RandomOwner(),
+		HashedPassword: "secret",
+		FullName:       util.RandomString(10),
+		Email:          util.RandomEmail(),
+	}
+
+	user, err := testQueries.CreateUser(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, arg.Username, user.Username)
+	require.Equal(t, arg.HashedPassword, user.HashedPassword)
+	require.Equal(t, arg.FullName, user.FullName)
+	require.Equal(t, arg.Email, user.Email)
+
+	require.True(t, user.PasswordChangedAt.IsZero())
+	require.NotZero(t, user.CreatedAt)
+
+	return user
+}
+
+func TestCreateUser(t *testing.T) {
+	CreateRandomUser(t)
+}
+
+func TestGetUser(t *testing.T) {
+	expectedUser := CreateRandomUser(t)
+	actualUser, err := testQueries.GetUser(context.Background(), expectedUser.Username)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, actualUser)
+	require.Equal(t, expectedUser.Username, actualUser.Username)
+	require.Equal(t, expectedUser.FullName, actualUser.FullName)
+	require.Equal(t, expectedUser.Email, actualUser.Email)
+	require.Equal(t, expectedUser.HashedPassword, actualUser.HashedPassword)
+	require.WithinDuration(t, expectedUser.PasswordChangedAt, actualUser.PasswordChangedAt, time.Second)
+	require.WithinDuration(t, expectedUser.CreatedAt, actualUser.CreatedAt, time.Second)
+}
--- util/random.go
+++ util/random.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"math/rand"
 	"strings"
 	"time"
@@ -40,4 +41,8 @@ func RandomCurrency() string {
 	currencies := []string{"USD", "CAD", "EUR"}
 
 	return currencies[rand.Intn(len(currencies))]
+}
+
+func RandomEmail() string {
+	return fmt.Sprintf("%s@gmail.com", RandomString(8))
 }
